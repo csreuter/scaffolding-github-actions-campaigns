713cfbb1ca10777bbc87ac4bf7bb641b6e1a9cf6
Chris Reuter <88755934+csreuter@users.noreply.github.com>
2025-02-05 10:24:03 -0500

    Update main.tf
    Adding RDS, Subnet, VPC, EC2 instance

--- main.tf
+++ main.tf
@@ -27,3 +27,65 @@ resource "aws_s3_bucket_versioning" "campaigns-resource_ckaMshbmm94dxCHA" {
     status = "Disabled"
   }
 }
+
+resource "aws_db_instance" "campaigns-resource_ckaMshbmm94dxCHA" {
+  allocated_storage    = 75
+  db_name              = "my-database"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  storage_encrypted    = false
+
+  timeouts {
+    create = "3h"
+    delete = "3h"
+    update = "3h"
+  }
+}
+
+resource "aws_vpc" "campaigns-resource_ckaMshbmm94dxCHA" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "campaigns-subnet-resource_ckaMshbmm94dxCHA" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "c6a.2xlarge"
+  subnet_id     = aws_subnet.campaigns-subnet-resource_ckaMshbmm94dxCHA.id
+
+  cpu_options {
+    core_count       = 2
+    threads_per_core = 2
+  }
+
+  tags = {
+    Name = "tf-example"
+  }
+}
