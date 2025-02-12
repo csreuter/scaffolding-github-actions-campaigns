91c8a49681c29d5a50b9fb8b7dfeffec9e7f0d5b
Aladdin Almubayed <522779+alaamub@users.noreply.github.com>
2025-02-12 08:55:18 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 # main.tf
 
-resource "aws_s3_bucket" "campaigns-resource_ckaMshbmm94dxCHA" {
+resource "aws_s3_bucket" "campaigns-resource_ckaMshbmm94dxMYO" {
   bucket = "campaigns-resource-test-bucket"
 }
 
-resource "aws_s3_bucket_public_access_block" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket                  = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_public_access_block" "campaigns-resource_ckaMshbmm94dxMYO" {
+  bucket                  = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxMYO.id
   block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_ownership_controls" "campaigns-resource_ckaMshbmm94dxMYO" {
+  bucket = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxMYO.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_versioning" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_versioning" "campaigns-resource_ckaMshbmm94dxMYO" {
+  bucket = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxMYO.id
 
   versioning_configuration {
     status = "Disabled"
