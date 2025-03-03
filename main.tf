9c2cfc49e34219f02f4329c8ca46c33d92da4126
Chris Reuter <88755934+csreuter@users.noreply.github.com>
2025-03-03 12:38:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,23 +35,3 @@ resource "aws_kms_key" "kmskey_6rqH9awtV732LCTF" {
   is_enabled              = true
   deletion_window_in_days = 30
 }
-
-# RDS Instance with Best Practices
-resource "aws_db_instance" "example-rds_kB9bGDNyJmy9WPbv" {
-  identifier                   = "example-rds"
-  engine                       = "postgres"
-  engine_version               = "17.2"
-  instance_class               = "db.t3.micro"
-  allocated_storage            = 20
-  max_allocated_storage        = 100
-  db_name                      = "app_database"
-  username                     = "db_user"
-  password                     = "password"
-  backup_retention_period      = 7
-  storage_encrypted            = false
-  kms_key_id                   = aws_kms_key.kmskey_6rqH9awtV732LCTF.arn
-  multi_az                     = true
-  deletion_protection          = true
-  performance_insights_enabled = true
-  skip_final_snapshot          = false
-}
