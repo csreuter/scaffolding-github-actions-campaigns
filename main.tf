1446a88704f5cf3725cb30eae7fb5d289bc6b9e7
Chris Reuter <88755934+csreuter@users.noreply.github.com>
2025-03-03 15:18:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,4 +53,5 @@ resource "aws_db_instance" "example-rds_TJXZTFi3qSy724Fv" {
   multi_az                     = true
   deletion_protection          = true
   performance_insights_enabled = true
+  skip_final_snapshot         = true
 }
